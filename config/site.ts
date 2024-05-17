44ac44863bf0bfcfd86bd0ef478f921f526f1934
mitesh_220650 <miteshpw22@iitk.ac.in>
2024-05-17 18:17:19 +0530

    added nav

---
+++ config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: "LC Board",
+  url: "https://ui.shadcn.com",
+  description: "Designing Whiteboard for different LeetCode Problems.",
+  links: {
+    twitter: "https://x.com/MiteshWandhare",
+  },
+}
+
+export type SiteConfig = typeof siteConfig

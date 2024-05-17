a5a0bd03cef17b384b030eb530775c9e3dabc26d
mitesh_220650 <miteshpw22@iitk.ac.in>
2024-05-17 18:21:14 +0530

    added nav-bar

--- src/components/ui/header/site-header.tsx
+++ src/components/ui/header/site-header.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import { ModeToggle } from "@/components/ui/mode-toggle"
+import { MainNav } from "@/components/ui/main-nav"
 
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-14 max-w-screen-2xl items-center">
+        <MainNav />
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
           <nav className="flex items-center" aria-label="Main Navigation">
             <ModeToggle />

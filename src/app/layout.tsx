e742dcecc7e582caf4ebdd011f06d735ffba4b81
mitesh_220650 <miteshpw22@iitk.ac.in>
2024-05-17 15:49:51 +0530

    fixed by prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,32 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/ui/theme-provide";
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import { ThemeProvider } from "@/components/ui/theme-provide"
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "LeetCode Board",
   description: "Online Whiteboarding for LeetCode Problems",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-     <html lang="en">
+    <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange> {children} </ThemeProvider></body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {" "}
+          {children}{" "}
+        </ThemeProvider>
+      </body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import { Button } from '@/components/ui/button';
-import { ModeToggle } from '@/components/ui/mode-toggle';
-import Header from '@/components/ui/header/header';
-import Footer from '@/components/ui/footer/footer';
+import React from "react"
+import { Button } from "@/components/ui/button"
+import { ModeToggle } from "@/components/ui/mode-toggle"
+import Header from "@/components/ui/header/header"
+import Footer from "@/components/ui/footer/footer"
 const HomePage = () => {
   return (
-    <div> <Header /> {/* Include the Header component */}
-    {/* Your page content goes here */}
-    <h1> Hello </h1>
-    <p>Just created the header.</p>
-    
-    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
-̊     
+    <div>
+      {" "}
+      <Header /> {/* Include the Header component */}
+      {/* Your page content goes here */}
+      <h1> Hello </h1>
+      <p>Just created the header.</p>
+      <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
+        ̊
+      </div>
+      <Footer /> {/* Include the Footer component */}
     </div>
-    <Footer /> {/* Include the Footer component */}
-    </div>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage

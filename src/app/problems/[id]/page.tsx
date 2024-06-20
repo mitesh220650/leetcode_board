c3f994aeb95b10ae22e3ab87cbd52c40ffbece62
mitesh_220650 <miteshpw22@iitk.ac.in>
2024-06-20 17:05:08 +0530

    added forms and problems specific page again to solve excalidraw issue

--- src/app/problems/[id]/page.tsx
+++ src/app/problems/[id]/page.tsx
@@ -1,15 +1,19 @@
-import dynamic from "next/dynamic"
-const Excalidraw = dynamic(
-  async () =>
-    (await import("../../../components/ui/custom/excalidraw-wrapper")).default,
-  {
-    ssr: false,
-  }
-)
-export default function App() {
+import React from "react"
+import { Button } from "@/components/ui/button"
+import { ModeToggle } from "@/components/ui/mode-toggle"
+
+const HomePage = () => {
   return (
-    <main>
-      <Excalidraw />
-    </main>
+    <div className="flex flex-col min-h-screen justify-between">
+      <main className="flex-grow flex flex-col justify-center overflow-hidden py-6 sm:py-12 relative">
+        <div className="flex items-center justify-center">
+          <div className="flex flex-row items space-x-4">
+            This is the problem specific page.
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
+
+export default HomePage

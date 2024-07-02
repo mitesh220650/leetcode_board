903d1f2769d54c80a4cc2a5d03a7c3e61728832c
mitesh220650 <121502354+mitesh220650@users.noreply.github.com>
2024-07-03 04:50:42 +0530

    Persistence Exercise, local storage added

--- src/components/ui/custom/excalidraw-wrapper.tsx
+++ src/components/ui/custom/excalidraw-wrapper.tsx
@@ -2,15 +2,33 @@
 import React, { useEffect, useState } from "react";
 
 import { Excalidraw, MainMenu, WelcomeScreen } from "@excalidraw/excalidraw"
+import {serializeAsJSON} from "@excalidraw/excalidraw"
 import { useTheme } from "next-themes";
 import "../../../components/ui/custom/excalidraw.css";
+import { AppState, BinaryFiles } from "@excalidraw/excalidraw/types/types"
+import { ExcalidrawElement } from "@excalidraw/excalidraw/types/element/types"
 
 const ExcalidrawWrapper: React.FC = () => {
   const { theme } = useTheme();
+  const onchange = (
+    elements: readonly ExcalidrawElement[],
+    appState: AppState,
+    files: BinaryFiles
+  )  => {
+    console.log("function invoked");
+    const content = serializeAsJSON(elements, appState, files, "local")
+    localStorage.setItem("excalidraw", content);
+  }
 
+  const retrieveInitialData = () => {
+    const content = localStorage.getItem("excalidraw")
+    if(content != null) {
+      return JSON.parse(content)
+    }
+  }
   return (
     <div className={`excalidraw-container ${theme === 'dark' ? 'dark' : 'light'}`}>
-      <Excalidraw theme={theme === 'dark' ? 'dark' : 'light'} >
+      <Excalidraw theme={theme === 'dark' ? 'dark' : 'light'} onChange = {onchange} initialData={retrieveInitialData()} >
         <WelcomeScreen>
           <WelcomeScreen.Hints.ToolbarHint/>
           <WelcomeScreen.Hints.HelpHint/>
